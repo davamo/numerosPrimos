c9054569fb5c5ea0ba1f7a9655612927199ac7e4
davamo <davamo@davamo.cl>
2021-03-13 22:44:53 -0300

    preliminar test unitarios

--- test/numerosPrimos-test.js
+++ test/numerosPrimos-test.js
@@ -6,9 +6,24 @@ var numerosPrimos = require('../numerosPrimos');
 
 describe('testin assert function', function(){
     describe('Chech numerosPrimos function', function(){
-        it('Check return value :  : ', function(){
+        it('Check return value : 7  : ', function(){
             result = numerosPrimos.addTest(7);
              assert.equal(result, [7, 5, 3, 2]); 
+            //assert.isAtLeast(7, 5, 3, 2); 
+            //expect(result).to.equal([7, 5, 3, 2]);
+            assert.isOk(result, result);
+
+            //assert(result !== [7, 5, 3, 2], result);
+
+        });
+    });
+})
+
+describe('testin assert function', function(){
+    describe('Chech numerosPrimos function', function(){
+        it('Check return value : 15 : ', function(){
+            result = numerosPrimos.addTest(15);
+             assert.equal(result, [ 13, 11, 7, 5, 3, 2 ]); 
             //assert.isAtLeast(7, 5, 3, 2); 
             //expect(result).to.equal([7, 5, 3, 2]);
             assert.isOk(result, result);

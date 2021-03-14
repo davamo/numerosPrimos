1453ae18283c67ed5ca8bec3d480126909633668
davamo <davamo@davamo.cl>
2021-03-13 23:30:22 -0300

    refactory v2 test unitarios

--- test/numerosPrimos-test.js
+++ test/numerosPrimos-test.js
@@ -6,7 +6,7 @@ var numerosPrimos = require("../numerosPrimos");
 
 describe("testing", () => {
   describe("Check numerosPrimos(7) ", () => {
-    it("return value : [7, 5, 3, 2]", () => {
+    it("return value : 7, 5, 3, 2", () => {
       result = numerosPrimos.addTest(7);
       expect(result).to.eql([7, 5, 3, 2]); 
     });
@@ -15,7 +15,7 @@ describe("testing", () => {
 
 describe("testing", () => {
   describe("Check numerosPrimos(15)", () => {
-    it("return value : [ 13, 11, 7, 5, 3, 2 ] ", () => {
+    it("return value :  13, 11, 7, 5, 3, 2", () => {
       result = numerosPrimos.addTest(15);
       expect(result).to.eql([13, 11, 7, 5, 3, 2]); 
     });
